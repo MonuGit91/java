fad42f14ec358d522d24b21842b0353b7888c40b
MonuGit91 <monuc20082000@gmail.com>
2022-07-06 20:58:46 +0530

    modify

--- dsa/src/com/recursion1/Summation.java
+++ dsa/src/com/recursion1/Summation.java
@@ -1,5 +1,13 @@
 package com.recursion1;
 
 public class Summation {
-
+	public static int summation(int n) {
+		if(n == 1) {
+			return 1;
+		}
+		return n+summation(n-1);
+	}
+	public static void main(String arg[]) {
+		System.out.println(summation(5));
+	}
 }
