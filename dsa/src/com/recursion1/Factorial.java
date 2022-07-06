3e889568b0cfc40258b3191d96151a0f1f8c78cc
MonuGit91 <monuc20082000@gmail.com>
2022-07-06 16:00:11 +0530

    factorial

--- dsa/src/com/recursion1/Factorial.java
+++ dsa/src/com/recursion1/Factorial.java
@@ -1,5 +1,14 @@
 package com.recursion1;
 
 public class Factorial {
-
+	public static int fact(int n) {
+		if(n == 0) {
+			return 1;
+		}
+		int smallAns = fact(n-1);
+		return n*smallAns;
+	}
+	public static void main(String[] arg) {
+		System.out.print(fact(5));
+	}
 }
